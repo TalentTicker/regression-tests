57d61a5b16ec2a019ef7c35c4ab82f4dc1d8e436
LukeSilverTT <90188189+LukeSilverTT@users.noreply.github.com>
2022-05-13 13:29:19 +0100

    Update sequence test name

--- playwright/tests/general_regression/sequencing.spec.js
+++ playwright/tests/general_regression/sequencing.spec.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 test.use({ storageState: 'tests/state.json' });
 
-test('Sequencing add new sequece - edit sequence - remove sequence', async ({ page }) => {
+test('Add, edit and remove a Sequence', async ({ page }) => {
   await page.goto(config.use.baseURL + "home");
 
   // Go to https://staging.talentticker.ai/en-GB/home
