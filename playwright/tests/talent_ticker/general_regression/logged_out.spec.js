50b5dbf36da5053545d02f84dd988c7637a81008
LukeSilverTT <90188189+LukeSilverTT@users.noreply.github.com>
2022-06-28 14:13:24 +0100

    Update logged_out.spec.js

--- playwright/tests/talent_ticker/general_regression/logged_out.spec.js
+++ playwright/tests/talent_ticker/general_regression/logged_out.spec.js
@@ -14,9 +14,11 @@ test('Logged out checks', async ({ page }) => {
   await page.click('li[role="option"]:has-text("Accounting")');
   await page.click('[data-test="searchSubmit"]');
 
-  await page.click('button:has-text("Sign Up")');
+  await page.click('button:has-text("Book A Demo")');
 
-  await expect(page).toHaveURL(config.use.baseURL + 'contact');
+  await expect(page).toHaveURL('https://www.talentticker.ai/contact');
+
+  await page.goto(config.use.baseURL);
 
   // Click [data-test="vacanciesNavButton"]
   await page.click('[data-test="vacanciesNavButton"]');
@@ -25,6 +27,8 @@ test('Logged out checks', async ({ page }) => {
   await page.click('button:has-text("Sign Up")');
 
   await expect(page).toHaveURL(config.use.baseURL + 'signup-online');
+
+  await page.click('[alt="Talent Ticker"]');
   
   // Click [data-testid="styled-anchor-tag"] div:has-text("Home")
   await Promise.all([
