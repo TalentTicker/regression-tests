53f64e65d9f0b994f947ecaa4d9f6babde4b5fe4
LukeSilverTT <90188189+LukeSilverTT@users.noreply.github.com>
2022-07-14 19:36:31 +0100

    Updating files
    Making sure they work in TS configuration

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -3,8 +3,11 @@ import { type PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  globalSetup: require.resolve('./tests/global-setup'),
   use: {
+    baseURL: "https://staging.talentticker.ai/",
     trace: 'on-first-retry',
+    storageState: './tests/state.json',
     headless: true,
     viewport: { width: 1920, height: 1080 },
     ignoreHTTPSErrors: true,
